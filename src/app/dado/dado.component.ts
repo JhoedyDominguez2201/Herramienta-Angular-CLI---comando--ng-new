b8a05b081df6f49cb492fd431fc2f937998caf93
JhoedyDominguez2201 <l17390333@chetumal.tecnm.mx>
2022-06-26 17:38:31 -0500

    Commit1

---
+++ src/app/dado/dado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ele-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
